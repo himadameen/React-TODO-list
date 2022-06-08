b1cedefc29c7cadb176e12bf16f636eb59d7f058
himmad ameen <ameenhh1620@gmail.com>
2022-06-08 11:55:47 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,97 +1,41 @@
-import React, { useState } from "react";
-import {nanoid} from 'nanoid';
-import "./../styles/App.css";
+import React, { Component, useState } from "react";
+import '../styles/App.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderBall: false,
+            posi : 0,
+            ballPosition: { left: "0px" }
+        };
+        this.renderChoice = this.renderBallOrButton.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+    };
 
-	const [state, setState] = useState("");
-	const [todo, setTodo] = useState([]);
-	const [edt, setEdit] = useState("");
-	const [edtValue, setEdtVal] = useState("");
-	const [save, setSave] = useState(false);
-
-
-
-	const handleChange = (e) => {
-		console.log(e.target.value);
-		const input = e.target.value;
-		setState(input);
-	}
-
-	const adding = () => {
-		let obj = {
-			id : nanoid(),
-			task: state,
-			status: false
+    buttonClickHandler() {
+   
+   }
+    renderBallOrButton() {
+		if (this.state.renderBall) {
+		    return <div className="ball" style={this.state.ballPosition}></div>
+		} else {
+		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
 		}
-		if(state !== ''){
-			setTodo([...todo, obj]);
-		console.log(todo);
-		setState('');
-		console.log(state);
-		}	
-	}
+    }
 
-	const edit = (data) => {
-		setEdit(data);
-		setSave(true);
-	}
+    // bind ArrowRight keydown event
+    componentDidMount() {
+      
+    }
 
-	const handleEdit = (e) => {
-		console.log(e.target.value);
-		setEdtVal(e.target.value);
-	}
-
-	const saveEdit = () => {
-		if(edtValue != ""){
-			setSave(false);
-			let temp = todo.map((item) => {
-				if(item.id === edt){
-					item.task = edtValue;
-				}
-				return item;
-			})
-			console.log(temp);
-			setTodo(temp);
-			setEdtVal('');
-			setEdit('');
-
-		}
-	}
-
-	const dele = (index) => {
-		let dlt = [...todo];
-		dlt.splice(index , 1);
-		setTodo(dlt);
-	}
-
-
-	return (
-
-		<div id="main">
-			<textarea value={state} id="task" onChange={handleChange}></textarea>
-			<button id="btn" onClick={adding}>Add</button>
-			<div>
-				<ul>
-					{todo && todo.map((item, index) => {
-						return (
-							<li className="list" key={index + 1}>{item.task}
-								<button onClick={() => edit(item.id)}>Edit</button>
-								<button onClick={() => dele(index)}>Delete</button>
-							</li>
-						)
-					})}
-
-					{save && (
-					<>
-					<textarea value={edtValue} className="editTask" onChange={handleEdit}></textarea>
-					<button className="saveTask" onClick={saveEdit} >Save</button>
-					</>
-					)}
-				</ul>
-			</div>
-		</div>
-	);
+    render() {
+        return (
+            <div className="playground">
+                {this.renderBallOrButton()}
+            </div>
+        )
+    }
 }
 
 
